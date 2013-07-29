0bd2be2a1d666d12a16794a1934d44a67c89e44f
Andre Peters <andre.peters.dev@googlemail.com>
2013-07-29 19:33:02 +0200

    add setter to configuration interfaces

--- jslider-root/jslider/src/main/java/com/apeters/vaadin/addon/jslider/InputTagConfiguration.java
+++ jslider-root/jslider/src/main/java/com/apeters/vaadin/addon/jslider/InputTagConfiguration.java
@@ -3,7 +3,11 @@ package com.apeters.vaadin.addon.jslider;
 public interface InputTagConfiguration {
 	
 	public abstract String getTagId();
-	
+
+	public abstract void setTagId(String tagId);
+
 	public abstract String getTagClass();
+
+	public abstract void setTagClass(String tagClass);
 	
 }
--- jslider-root/jslider/src/main/java/com/apeters/vaadin/addon/jslider/SliderConfiguration.java
+++ jslider-root/jslider/src/main/java/com/apeters/vaadin/addon/jslider/SliderConfiguration.java
@@ -7,25 +7,45 @@ import com.apeters.vaadin.addon.jslider.shared.Range;
 public interface SliderConfiguration {
 	
 	public abstract Range getRange();
-	
+
+	public abstract void setRange(Range range);
+
 	public abstract Number getStep();
-	
+
+	public abstract void setStep(Number step);
+
 	public abstract Number getRound();
-	
+
+	public abstract void setRound(Number round);
+
 	public abstract Format getFormat();
-	
+
+	public abstract void setFormat(Format format);
+
 	public abstract String getDimension();
-	
+
+	public abstract void setDimension(String dimension);
+
 	public abstract Heterogeneity getHeterogeneity();
-	
+
+	public abstract void setHeterogeneity(Heterogeneity heterogeneity);
+
 	public abstract boolean isLimits();
-	
+
+	public abstract void setLimits(boolean limits);
+
 	public abstract Object[] getScale();
-	
+
+	public abstract void setScale(Object[] scale);
+
 	public abstract String getSkin();
-	
+
+	public abstract void setSkin(String skin);
+
 	public abstract String getFormatString();
-	
+
+	public abstract void setCalculate(String calculate);
+
 	public abstract String getCalculate();
 	
 }
